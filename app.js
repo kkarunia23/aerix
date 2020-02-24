19c5b9175284207ae51c2ed9b93a063cc18321fe
kkarunia23 <kenneth.karunia@gmail.com>
2020-02-24 22:33:41 +0800

    corsss

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const mongoose = require('mongoose');
 const mongoURI = 'mongodb://localhost:27017/aerisdb';
 const Feed = require('./models/feed.js');
 const db = mongoose.connection;
+var cors = require('cors');
+var app = express();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 mongoose.connect(mongoURI, () => console.log('Mongo running at', mongoURI));
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', mongoURI));
--- app.ts
+++ app.ts
@@ -11,6 +11,16 @@ const mongoURI = 'mongodb://localhost:27017/aerisdb';
 const Feed = require('./models/feed.js');
 const db = mongoose.connection;
 
+var cors = require('cors');
+var app = express();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 mongoose.connect(mongoURI,
     () => console.log('Mongo running at', mongoURI)
 );
